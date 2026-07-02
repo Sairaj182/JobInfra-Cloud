d117cca68d13e25987d5afd54af2d0e885e0cd2c
Sairaj182 <sairajraithatha.official@gmail.com>
2026-07-02 16:49:10 +0530

    Fix proxy issues

--- jobinfra-server/src/main/java/com/sairaj/jobinfra/server/config/OpenApiConfig.java
+++ jobinfra-server/src/main/java/com/sairaj/jobinfra/server/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("JobInfra Cloud API")
                         .version("1.0.0")
